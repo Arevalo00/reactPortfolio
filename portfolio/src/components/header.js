690fd1e26da0b77f98da678df0d4379b4b4fd243
jennifer arevalo <arevalo5007@gmail.com>
2023-11-08 15:03:03 -0600

    insall tailwind css

--- portfolio/src/components/header.js
+++ portfolio/src/components/header.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
+    <header className="bg-gray text-light mb-4 py-3 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
-        <h1 className="m-0">Jennifer Arevalo Portfolio</h1>
+        <h1 className="m-0 ">Jennifer Arevalo Portfolio</h1>
         <p className="m-0"></p>
       </div>
     </header>
